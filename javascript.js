be06838f205707791853251d67590b6bff8e34a6
Zhan <zhanxiangzheng136@gmail.com>
2022-11-17 09:13:35 -0500

    commit #1

--- javascript.js
+++ javascript.js
@@ -4,17 +4,24 @@ function initialize(){
 
 }
 
-function addItem(){
-
+function addItem(item){
+    if (!checkDup(item)) groceryList.push(item);
 }
 
-function removeItem(){
-
+function removeItem(idx){
+    groceryList.slice(idx, 1);
 }
 
 function moveItem(){
 
 } 
+
+function checkDup(item) {
+    if (groceryList.indexOf(item) > -1) {
+        return true;
+    }
+    return false;
+}
 
 
 
