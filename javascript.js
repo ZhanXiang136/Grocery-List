dd561efaba7fdea9064c87656eacb32023d23675
Q <bths@bths.edu>
2022-11-21 08:37:05 -0500

    Added initialize

--- javascript.js
+++ javascript.js
@@ -4,11 +4,20 @@ document.getElementById("addButton").addEventListener("click", addItem());
 document.getElementById("deleteButton").addEventListener("click:", removeItem());
 
 function initialize(){
+    add = document.getElementById("add");
+    move = document.getElementById("move");
+    addItem = add.addItem.addItem;
+    moveItem = move.moveItem.moveItem;
+    display = document.getElementById("display");
+}
+
+function display(){
 
 }
 
 function addItem(item){
-    if (!checkDup(item)) groceryList.push(item);
+    if (!checkDup(item)) 
+    groceryList.push(item);
 }
 
 function removeItem(idx){
