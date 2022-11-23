b2f4c2445f25dc184118d921a21e6f7b67c47587
Z <zhanxiangzheng136@gmail.com>
2022-11-23 09:02:43 -0500

    Fixed move up error

--- javascript.js
+++ javascript.js
@@ -1,21 +1,24 @@
-let groceryList = [];
+let groceryList = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
 let addorDeleteform = document.getElementById("addDelete");
 let addorDelete = document.getElementById("addorDeleteItemInput");
-let move = document.getElementById("moveItem")
+let move = document.getElementById("moveItem");
 let displayList = document.getElementById("display");
-// let count = 1;
+let moveIdx = null;
 
 document.getElementById("addButton").addEventListener("click", addItem);
 document.getElementById("deleteButton").addEventListener("click", removeItem);
 document.getElementById("moveUpButton").addEventListener("click", moveItemUp);
-// document.getElementById("moveUpButton").addEventListener("change", counter(true));
+document.getElementById("moveDownButton").addEventListener("click", moveItemDown);
+
+display();
+
 
 function display(){
     displayList.innerHTML = "0: " + groceryList[0];
     for (let i = 1; i < groceryList.length; i++) {
         displayList.innerHTML += "<br />" + i + ": " + groceryList[i];
     }
-}
+};
 
 function addItem(){
     let item = addorDelete.value;
@@ -37,25 +40,37 @@ function removeItem(){
 
 function moveItemUp(){
     let index = move.value;
-    let temp = groceryList.pop(groceryList[index])
-    groceryList.splice(index, 0, temp);
-    count++;
-    display();
 
-    // let index = move.value;
-    // let temp = groceryList.pop(groceryList[index - count])
-    // groceryList.splice(index - count, 0, temp);
-    // count++;
-    // display();
+    if (moveIdx === null || moveIdx > index) 
+    {
+        moveIdx = index;
+    }
+
+    moveIdx < index ? swapElement(moveIdx, moveIdx-1) : swapElement(moveIdx, moveIdx-1)
+
+    moveIdx--;
+    display();
 }
 
 function moveItemDown(){
+    let index = move.value;
 
+    if (moveIdx === null || moveIdx < index) 
+    {
+        moveIdx = index;
+    }
+
+    moveIdx < index ? swapElement(moveIdx, moveIdx+1) : swapElement(moveIdx, moveIdx+1)
+
+    moveIdx--;
+    display();
 }
 
-// function counter(truthValue){
-//     if (truthValue){
-//         count= 1;
-//     }
-// }
+function swapElement(idx1, idx2) {
+    let temp = groceryList[idx1];
+    groceryList[idx1] = groceryList[idx2];
+    groceryList[idx2] = temp;
+}
 
+//let temp = groceryList.pop(groceryList[index])
+//groceryList.splice(index - 1, 0, temp);
