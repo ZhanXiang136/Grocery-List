5e4f2f3733e9ce30a3b8dcb8f43de5f59d28e53c
quntao <quntao.zheng1027@gmail.com>
2022-11-27 20:29:04 -0500

    swapElement more efficent

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
-let groceryList = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
-let addorDeleteform = document.getElementById("addDelete");
+let groceryList = ["a", "b", "c", "d", "e", "f", "g"];
+let addorDeleteform = document.getElementById("addDeleteForm");
 let addorDelete = document.getElementById("addorDeleteItemInput");
 let move = document.getElementById("moveItem");
 let displayList = document.getElementById("display");
@@ -12,69 +12,66 @@ document.getElementById("moveDownButton").addEventListener("click", moveItemDown
 
 display();
 
-function display(){
-    displayList.innerHTML = "0: " + groceryList[0];
-    for (let i = 1; i < groceryList.length; i++) {
-        displayList.innerHTML += "<br />" + i + ": " + groceryList[i];
-    }
-};
-
-function addItem(){
-    let item = addorDelete.value;
-    if (groceryList.indexOf(item) === -1) {
-        groceryList.push(item);
-    }
-    display();
-    addorDeleteform.reset();
+function display() {
+	displayList.innerHTML = "0: " + groceryList[0];
+	for (let i = 1; i < groceryList.length; i++) {
+		displayList.innerHTML += "<br />" + i + ": " + groceryList[i];
+	}
 }
 
-function removeItem(){
-    let item = addorDelete.value;
-    if (groceryList.indexOf(item) > -1) {
-        groceryList.splice(groceryList.indexOf(item), 1);
-    }
-    display();
-    addorDeleteform.reset();
+function addItem() {
+	let item = addorDelete.value;
+	if (groceryList.indexOf(item) === -1) {
+		groceryList.push(item);
+	}
+	display();
+	addorDeleteform.reset();
 }
 
-function moveItemUp(){
-    let index = parseInt(move.value);
-
-    if (moveIdx === null || moveIdx > index) 
-    {
-        moveIdx = index;
-    }
-    if (moveIdx === 0) {
-        alert("Can't move up anymore");
-        return;
-    }
-
-    swapElement(moveIdx, moveIdx-1)
-    moveIdx--;
-    display();
+function removeItem() {
+	let item = addorDelete.value;
+	if (groceryList.indexOf(item) > -1) {
+		groceryList.splice(groceryList.indexOf(item), 1);
+	}
+	display();
+	addorDeleteform.reset();
 }
 
-function moveItemDown(){
-    let index = parseInt(move.value);
+function moveItemUp() {
+	let index = parseInt(move.value);
 
-    if (moveIdx === null || moveIdx < index) 
-    {
-        moveIdx = index;
-    }
-    if (moveIdx === groceryList.length-1) {
-        alert("Can't move down anymore");
-        return;
-    }
+	if (moveIdx === null || moveIdx > index) {
+		moveIdx = index;
+	}
+	if (moveIdx === 0) {
+		alert("Can't move up anymore");
+		return;
+	}
 
-    swapElement(moveIdx, moveIdx+1)
-    moveIdx++;
-    display();
+	swapElement(moveIdx, moveIdx - 1);
+	moveIdx--;
+	display();
+
+	console.log(moveIdx + "and" + index);
+}
+
+function moveItemDown() {
+	console.log(moveIdx);
+	let index = parseInt(move.value);
+
+	if (moveIdx === null || moveIdx < index) {
+		moveIdx = index;
+	}
+	if (moveIdx === groceryList.length - 1) {
+		alert("Can't move down anymore");
+		return;
+	}
+
+	swapElement(moveIdx, moveIdx + 1);
+	moveIdx++;
+	display();
 }
 
 function swapElement(idx1, idx2) {
-    console.log(idx1, idx2)
-    let temp = groceryList[idx1];
-    groceryList[idx1] = groceryList[idx2];
-    groceryList[idx2] = temp;
+    groceryList[idx1] = groceryList.splice(idx2, 1, groceryList[idx1])[0];
 }
-
